fdb59188e0b4d8ce38e5c3fad7315cbc6def98a6
fifinurkholifah3-arch <fifinurkholifah3@gmail.com>
2026-02-05 11:37:26 +0700

    menambahkan latihan1

--- latihan_2407059/src/latihan_pbo/latihan1/Main.java
+++ latihan_2407059/src/latihan_pbo/latihan1/Main.java
@@ -11,13 +11,21 @@ class Cafe{
         System.out.println("nama cafenya adalah " + this.Nama);
         System.out.println("alamatnya ada di daerah: " + this.Alamat);
     }
+    void buka1(){
+        System.out.println(this.Nama + " cafe buka jam 10 pagi");
+    }
+    void buka2(){
+        System.out.println(this.Nama + " cafe buka jam 7 pagi");
+    }
 }
 
 //kelas main adalah untuk menampilkan program yang akan kita buat
 public class Main {
     public static void main(String[] args) {
         Cafe cafe1 = new Cafe("Kopi Kenangan", "Indramayu");
+        cafe1.buka1();
         Cafe cafe2 = new Cafe("Tomoro Coffee", "Indramayu");
+        cafe2.buka2();
         // cafe1.Nama = "Kopi Kenangan";
         // cafe1.Alamat = "Indramayu";
         // cafe2.Nama = "kopte";
